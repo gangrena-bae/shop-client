8a6b686b7af437a9e5073a7efad928696018f5f0
gangrena-bae <woodman.mail.me@gmail.com>
2024-02-09 08:15:31 +0200

    мессадж

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
-app.use(express.static(__dirname))
-app.use(express.static(path.resolve(__dirname, 'build')))
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, "build")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-app.listen(PORT)
+app.listen(PORT);
